c0ff3259e787be538b2b85f6ab040c2e05463c46
姚文强 <yaowenqiang111@163.com>
2022-09-30 22:35:54 +0800

    add more oop demo

--- src/exercises/OOBasics.scala
+++ src/exercises/OOBasics.scala
@@ -6,6 +6,18 @@ object OOBasics extends App {
     println(person.x)
     person.greet("Jack")
     person.greet()
+
+    val author = new Writer("Charies", "Dickens", 1812)
+    val novel = new Novel("Great Expectations", 1861, author)
+    println(novel.authorAge)
+    println(novel.isWrittenBy(author))
+
+    val counter = new Counter()
+    counter.inc.print
+    counter.inc.inc.inc.inc.print
+    counter.inc(10).print
+
+
 }
 
 class Person(name: String, val age: Int) {
@@ -17,4 +29,43 @@ class Person(name: String, val age: Int) {
 
     def this(name: String) = this(name, 0)
     def this() = this("foo")
+
+
+}
+
+class Writer(firstName: String, surName: String, val year: Int) {
+    def fullName : String = firstName + " " + surName
+}
+
+class Novel(name: String, year: Int, author: Writer) {
+    def authorAge = year - author.year
+    def isWrittenBy(author: Writer) =  author == this.author
+    def copy(newYear: Int) : Novel = new Novel(name, newYear, author)
+}
+
+class Counter(val count: Int = 0) {
+//   def count = count
+    def inc = {
+        println("Incrementing")
+        new Counter(count + 1)
+    }
+    def dec = {
+        println("Decrementing")
+        new Counter(count - 1)
+    }
+
+    //    def inc(n: Int) = new Counter(count + n)
+    //    def dec(n: Int) = new Counter(count - n)
+    def inc(n: Int): Counter = {
+        if (n <= 0) this
+        else inc.inc(n-1)
+    }
+    def dec(n: Int): Counter = {
+        if (n <= 0) this
+        else dec.dec(n-1)
+    }
+
+    def print = println(count)
+
+
 }
