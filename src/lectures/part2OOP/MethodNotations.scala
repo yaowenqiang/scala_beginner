c87f25f82cf58d26566db5240998d1fed0b26861
姚文强 <yaowenqiang111@163.com>
2022-10-01 09:15:26 +0800

    add more method notations demo

--- src/lectures/part2OOP/MethodNotations.scala
+++ src/lectures/part2OOP/MethodNotations.scala
@@ -3,13 +3,19 @@ import scala.language.postfixOps
 
 
 object MethodNotations extends  App {
-    class Person(val name: String, movie : String) {
+    class Person(val name: String, movie : String, val age: Int = 0) {
         def likes(movie: String) : Boolean = movie == this.movie
         def hangoutWith(person: Person) :String = s"${this.name} hanging out with ${person.name}"
         def +(person: Person) :String = s"${this.name} hanging out with ${person.name}"
         def unary_! : String = s"$name, what the hack??"
         def isAlive: Boolean = true
         def apply() : String =  s"Hi, my name = ${name} and i like ${movie}"
+        def apply(n: Int) : String =  s"${name} watched ${movie} ${n} times"
+        def +(nick: String) :Person = new Person(s"${name} ${nick}", movie)
+        def unary_+ : Person = new Person(name, movie, age+1)
+        def learn(thing :String) = s"${name} is learning ${thing}"
+        def learnScala = learn("Scala")
+
     }
 
     def mary = new Person("Mary", "Inception")
@@ -24,9 +30,16 @@ object MethodNotations extends  App {
     println(mary hangoutWith john)
     println(mary + john)
     println(mary.+(john))
+    println((mary + "SuperStart")())
+    println((mary + "SuperStart").apply())
     println(1 + 2)
     println(1.+(2))
     println(!mary)
+    println("=========")
+    println((+mary).age)
+    println("=========")
+    println(mary.learnScala)
+    println(mary(10))
 
 
     //ALL OPERATORS ARE METHODS
