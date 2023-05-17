1b49a08024f8ac029d31ad12155558df9458b8d6
yaowenqiang <yaowenqiang111@163.com>
2023-05-17 23:38:26 +0800

    add scala streams demo

---
+++ src/lectures/streams/streams.scala
@@ -0,0 +1,7 @@
+import scala.math.BigInt
+object Main extends App {
+
+    lazy val fibs: Stream[BigInt] = BigInt(0) #:: BigInt(1) #:: fibs.zip(fibs.tail).map { n => n._1 + n._2 }
+
+      fibs take 5 foreach println
+}
